e9b38555c002c0156cfdd1d2564298295928fd79
labcabrera <lab.cabrera@gmail.com>
2018-04-21 09:00:34 +0200

    Initial

--- insurance-sso-server/src/main/java/org/lab/insurance/oauth/server/CorsFilter.java
+++ insurance-sso-server/src/main/java/org/lab/insurance/oauth/server/CorsFilter.java
@@ -21,8 +21,8 @@ public class CorsFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		
+		throws IOException, ServletException {
+
 		final HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
